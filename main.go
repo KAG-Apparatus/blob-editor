24ad7cc095723e5092747782cecafc7b51db2eb0
joaowiciuk <joaowiciuk@hotmail.com>
2020-07-18 20:53:54 -0500

    Astilectron demo

--- main.go
+++ main.go
@@ -1,50 +1,104 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/asticode/go-astikit"
 	"github.com/asticode/go-astilectron"
+	bootstrap "github.com/asticode/go-astilectron-bootstrap"
+)
+
+// Constants
+const htmlAbout = `Welcome on <b>Astilectron</b> demo!<br>
+This is using the bootstrap and the bundler.`
+
+// Vars injected via ldflags by bundler
+var (
+	AppName            string
+	BuiltAt            string
+	VersionAstilectron string
+	VersionElectron    string
+)
+
+// Application Vars
+var (
+	fs    = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	debug = fs.Bool("d", false, "enables the debug mode")
+	w     *astilectron.Window
 )
 
 func main() {
-	// Set logger
+	// Create logger
 	l := log.New(log.Writer(), log.Prefix(), log.Flags())
 
-	// Create astilectron
-	a, err := astilectron.New(l, astilectron.Options{
-		AppName:           "Test",
-		BaseDirectoryPath: "example",
-	})
-	if err != nil {
-		l.Fatal(fmt.Errorf("main: creating astilectron failed: %w", err))
-	}
-	defer a.Close()
+	// Parse flags
+	fs.Parse(os.Args[1:])
 
-	// Handle signals
-	a.HandleSignals()
-
-	// Start
-	if err = a.Start(); err != nil {
-		l.Fatal(fmt.Errorf("main: starting astilectron failed: %w", err))
-	}
-
-	// New window
-	var w *astilectron.Window
-	if w, err = a.NewWindow("resources/index.html", &astilectron.WindowOptions{
-		Center: astikit.BoolPtr(true),
-		Height: astikit.IntPtr(700),
-		Width:  astikit.IntPtr(700),
+	// Run bootstrap
+	l.Printf("Running app built at %s\n", BuiltAt)
+	if err := bootstrap.Run(bootstrap.Options{
+		Asset:    Asset,
+		AssetDir: AssetDir,
+		AstilectronOptions: astilectron.Options{
+			AppName:            AppName,
+			AppIconDarwinPath:  "resources/icon.icns",
+			AppIconDefaultPath: "resources/icon.png",
+			SingleInstance:     true,
+			VersionAstilectron: VersionAstilectron,
+			VersionElectron:    VersionElectron,
+		},
+		Debug:  *debug,
+		Logger: l,
+		MenuOptions: []*astilectron.MenuItemOptions{{
+			Label: astikit.StrPtr("File"),
+			SubMenu: []*astilectron.MenuItemOptions{
+				{
+					Label: astikit.StrPtr("About"),
+					OnClick: func(e astilectron.Event) (deleteListener bool) {
+						if err := bootstrap.SendMessage(w, "about", htmlAbout, func(m *bootstrap.MessageIn) {
+							// Unmarshal payload
+							var s string
+							if err := json.Unmarshal(m.Payload, &s); err != nil {
+								l.Println(fmt.Errorf("unmarshaling payload failed: %w", err))
+								return
+							}
+							l.Printf("About modal has been displayed and payload is %s!\n", s)
+						}); err != nil {
+							l.Println(fmt.Errorf("sending about event failed: %w", err))
+						}
+						return
+					},
+				},
+				{Role: astilectron.MenuItemRoleClose},
+			},
+		}},
+		OnWait: func(_ *astilectron.Astilectron, ws []*astilectron.Window, _ *astilectron.Menu, _ *astilectron.Tray, _ *astilectron.Menu) error {
+			w = ws[0]
+			go func() {
+				time.Sleep(5 * time.Second)
+				if err := bootstrap.SendMessage(w, "check.out.menu", "Don't forget to check out the menu!"); err != nil {
+					l.Println(fmt.Errorf("sending check.out.menu event failed: %w", err))
+				}
+			}()
+			return nil
+		},
+		RestoreAssets: RestoreAssets,
+		Windows: []*bootstrap.Window{{
+			Homepage:       "index.html",
+			MessageHandler: handleMessages,
+			Options: &astilectron.WindowOptions{
+				BackgroundColor: astikit.StrPtr("#333"),
+				Center:          astikit.BoolPtr(true),
+				Height:          astikit.IntPtr(700),
+				Width:           astikit.IntPtr(700),
+			},
+		}},
 	}); err != nil {
-		l.Fatal(fmt.Errorf("main: new window failed: %w", err))
+		l.Fatal(fmt.Errorf("running bootstrap failed: %w", err))
 	}
-
-	// Create windows
-	if err = w.Create(); err != nil {
-		l.Fatal(fmt.Errorf("main: creating window failed: %w", err))
-	}
-
-	// Blocking pattern
-	a.Wait()
 }
--- resources/generate.go
+++
@@ -1,4 +0,0 @@
-//go:generate file2byteslice -package=images -input=./images/ui.png -output=./images/ui.go -var=UI_png
-//go:generate gofmt -s -w .
-
-package resources
