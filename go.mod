70972c656609c9e730b8e3fbb615629274392751
joaowiciuk <joaowiciuk@hotmail.com>
2020-07-19 16:52:39 -0500

    Added tabs

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.14
 
 require (
 	github.com/akavel/rsrc v0.9.0 // indirect
-	github.com/asticode/go-astichartjs v0.1.0
 	github.com/asticode/go-astikit v0.8.0
 	github.com/asticode/go-astilectron v0.16.0
 	github.com/asticode/go-astilectron-bootstrap v0.4.1
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/asticode/go-astikit"
 	"github.com/asticode/go-astilectron"
@@ -52,41 +50,8 @@ func main() {
 			VersionAstilectron: VersionAstilectron,
 			VersionElectron:    VersionElectron,
 		},
-		Debug:  *debug,
-		Logger: l,
-		MenuOptions: []*astilectron.MenuItemOptions{{
-			Label: astikit.StrPtr("File"),
-			SubMenu: []*astilectron.MenuItemOptions{
-				{
-					Label: astikit.StrPtr("About"),
-					OnClick: func(e astilectron.Event) (deleteListener bool) {
-						if err := bootstrap.SendMessage(w, "about", htmlAbout, func(m *bootstrap.MessageIn) {
-							// Unmarshal payload
-							var s string
-							if err := json.Unmarshal(m.Payload, &s); err != nil {
-								l.Println(fmt.Errorf("unmarshaling payload failed: %w", err))
-								return
-							}
-							l.Printf("About modal has been displayed and payload is %s!\n", s)
-						}); err != nil {
-							l.Println(fmt.Errorf("sending about event failed: %w", err))
-						}
-						return
-					},
-				},
-				{Role: astilectron.MenuItemRoleClose},
-			},
-		}},
-		OnWait: func(_ *astilectron.Astilectron, ws []*astilectron.Window, _ *astilectron.Menu, _ *astilectron.Tray, _ *astilectron.Menu) error {
-			w = ws[0]
-			go func() {
-				time.Sleep(5 * time.Second)
-				if err := bootstrap.SendMessage(w, "check.out.menu", "Don't forget to check out the menu!"); err != nil {
-					l.Println(fmt.Errorf("sending check.out.menu event failed: %w", err))
-				}
-			}()
-			return nil
-		},
+		Debug:         *debug,
+		Logger:        l,
 		RestoreAssets: RestoreAssets,
 		Windows: []*bootstrap.Window{{
 			Homepage:       "index.html",
