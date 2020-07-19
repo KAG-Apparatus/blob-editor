76629e7555e61185dda46c7bf168f555b80d5ca2
joaowiciuk <joaowiciuk@hotmail.com>
2020-07-18 19:54:18 -0500

    Switched to astilectron

--- go.mod
+++ go.mod
@@ -3,11 +3,8 @@ module github.com/KAG-Apparatus/bounding-box
 go 1.14
 
 require (
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200707082815-5321531c36a2 // indirect
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
-	github.com/hajimehoshi/ebiten v1.11.4
-	golang.org/x/exp v0.0.0-20200513190911-00229845015e // indirect
-	golang.org/x/image v0.0.0-20200618115811-c13761719519
-	golang.org/x/mobile v0.0.0-20200629153529-33b80540585f // indirect
-	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+	github.com/asticode/go-astikit v0.8.0
+	github.com/asticode/go-astilectron v0.16.0
+	github.com/kr/pretty v0.1.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- main.go
+++ main.go
@@ -1,141 +1,50 @@
 package main
 
 import (
-	"bytes"
-	"image"
-	_ "image/png"
-	"io/ioutil"
+	"fmt"
 	"log"
 
-	"github.com/KAG-Apparatus/bounding-box/resources/images"
-	"github.com/golang/freetype/truetype"
-	"golang.org/x/image/font"
-
-	"github.com/hajimehoshi/ebiten"
+	"github.com/asticode/go-astikit"
+	"github.com/asticode/go-astilectron"
 )
-
-const (
-	lineHeight = 16
-)
-
-var (
-	uiImage       *ebiten.Image
-	uiFont        font.Face
-	uiFontMHeight int
-)
-
-func init() {
-	img, _, err := image.Decode(bytes.NewReader(images.UI_png))
-	if err != nil {
-		log.Fatal(err)
-	}
-	uiImage, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
-
-	bytes, err := ioutil.ReadFile("./resources/fonts/Roboto-Medium.ttf")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	tt, err := truetype.Parse(bytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-	uiFont = truetype.NewFace(tt, &truetype.Options{
-		Size:    12,
-		DPI:     72,
-		Hinting: font.HintingFull,
-	})
-
-	b, _, _ := uiFont.GlyphBounds('M')
-	uiFontMHeight = (b.Max.Y - b.Min.Y).Ceil()
-}
-
-type imageType int
-
-const (
-	imageTypeButton imageType = iota
-	imageTypeButtonPressed
-	imageTypeTextBox
-	imageTypeVScollBarBack
-	imageTypeVScollBarFront
-	imageTypeCheckBox
-	imageTypeCheckBoxPressed
-	imageTypeCheckBoxMark
-)
-
-var imageSrcRects = map[imageType]image.Rectangle{
-	imageTypeButton:          image.Rect(0, 0, 16, 16),
-	imageTypeButtonPressed:   image.Rect(16, 0, 32, 16),
-	imageTypeTextBox:         image.Rect(0, 16, 16, 32),
-	imageTypeVScollBarBack:   image.Rect(16, 16, 24, 32),
-	imageTypeVScollBarFront:  image.Rect(24, 16, 32, 32),
-	imageTypeCheckBox:        image.Rect(0, 32, 16, 48),
-	imageTypeCheckBoxPressed: image.Rect(16, 32, 32, 48),
-	imageTypeCheckBoxMark:    image.Rect(32, 32, 48, 48),
-}
-
-const (
-	screenWidth  = 640
-	screenHeight = 480
-)
-
-func drawNinePatches(dst *ebiten.Image, dstRect image.Rectangle, srcRect image.Rectangle) {
-	srcX := srcRect.Min.X
-	srcY := srcRect.Min.Y
-	srcW := srcRect.Dx()
-	srcH := srcRect.Dy()
-
-	dstX := dstRect.Min.X
-	dstY := dstRect.Min.Y
-	dstW := dstRect.Dx()
-	dstH := dstRect.Dy()
-
-	op := &ebiten.DrawImageOptions{}
-	for j := 0; j < 3; j++ {
-		for i := 0; i < 3; i++ {
-			op.GeoM.Reset()
-
-			sx := srcX
-			sy := srcY
-			sw := srcW / 4
-			sh := srcH / 4
-			dx := 0
-			dy := 0
-			dw := sw
-			dh := sh
-			switch i {
-			case 1:
-				sx = srcX + srcW/4
-				sw = srcW / 2
-				dx = srcW / 4
-				dw = dstW - 2*srcW/4
-			case 2:
-				sx = srcX + 3*srcW/4
-				dx = dstW - srcW/4
-			}
-			switch j {
-			case 1:
-				sy = srcY + srcH/4
-				sh = srcH / 2
-				dy = srcH / 4
-				dh = dstH - 2*srcH/4
-			case 2:
-				sy = srcY + 3*srcH/4
-				dy = dstH - srcH/4
-			}
-
-			op.GeoM.Scale(float64(dw)/float64(sw), float64(dh)/float64(sh))
-			op.GeoM.Translate(float64(dx), float64(dy))
-			op.GeoM.Translate(float64(dstX), float64(dstY))
-			dst.DrawImage(uiImage.SubImage(image.Rect(sx, sy, sx+sw, sy+sh)).(*ebiten.Image), op)
-		}
-	}
-}
 
 func main() {
-	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Blob Editor")
-	if err := ebiten.RunGame(&g); err != nil {
-		log.Fatal(err)
+	// Set logger
+	l := log.New(log.Writer(), log.Prefix(), log.Flags())
+
+	// Create astilectron
+	a, err := astilectron.New(l, astilectron.Options{
+		AppName:           "Test",
+		BaseDirectoryPath: "example",
+	})
+	if err != nil {
+		l.Fatal(fmt.Errorf("main: creating astilectron failed: %w", err))
 	}
+	defer a.Close()
+
+	// Handle signals
+	a.HandleSignals()
+
+	// Start
+	if err = a.Start(); err != nil {
+		l.Fatal(fmt.Errorf("main: starting astilectron failed: %w", err))
+	}
+
+	// New window
+	var w *astilectron.Window
+	if w, err = a.NewWindow("resources/index.html", &astilectron.WindowOptions{
+		Center: astikit.BoolPtr(true),
+		Height: astikit.IntPtr(700),
+		Width:  astikit.IntPtr(700),
+	}); err != nil {
+		l.Fatal(fmt.Errorf("main: new window failed: %w", err))
+	}
+
+	// Create windows
+	if err = w.Create(); err != nil {
+		l.Fatal(fmt.Errorf("main: creating window failed: %w", err))
+	}
+
+	// Blocking pattern
+	a.Wait()
 }
