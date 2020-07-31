2dec9b53f764425371f19d3a69c0d15d025ed454
Zable-the-Unable <35090781+Zable-the-Unable@users.noreply.github.com>
2020-07-30 18:01:05 -0700

    Did some basic styling and setup to mimic previous blob-editor

---
+++ Btn-Exit.gd
@@ -0,0 +1,8 @@
+extends Button
+
+func _ready():
+	pass
+
+
+func _on_BtnExit_pressed():
+	get_tree().quit();
---
+++ Btn-Open.gd
@@ -0,0 +1,12 @@
+extends Button
+
+func _ready():
+	pass
+
+
+func _on_BtnOpen_pressed():
+	$FileDialog.popup_centered();
+
+
+func _on_BtnExit_pressed():
+	pass # Replace with function body.
